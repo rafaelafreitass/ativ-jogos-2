7e2ff01885242a513b714b5bd4f9b00e0a88d15f
dutsn <defreitasrafaela959@gmail.com>
2026-06-02 11:59:01 -0300

    .

---
+++ jogos 2 info161/Assets/TargetSpawner.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+
+public class TargetSpawner : MonoBehaviour
+{
+    [HideInInspector]
+    public TargetSpawner.SpawnPoint spawnPoint;
+
+    [HideInInspector]
+    public bool moveHorizontal = false;
+    [HideInInspector]
+    public bool moveVertical = false;
+    [HideInInspector]
+    public float moveSpeed = 3f;
+    [HideInInspector]
+    public float moveRange = 5f;
+    [HideInInspector]
+    public int health = 1;
+    [HideInInspector]
+    public int pointsValue = 10;
+
+    private Vector3 startPosition;
+    private float directionX = 1f;
+    private float directionY = 1f;
+    private float directionZ = 1f; // Nota: Embora não apareça explicitamente na declaração cortada, as variáveis seguem o padrão X e Y.
+    private FPSAimController playerShooter;
+
+    void Start()
+    {
+        startPosition = transform.position;
+        playerShooter = FindObjectOfType<FPSAimController>();
+    }
+
+    void Update()
+    {
+        // Movimento
+        Vector3 newPos = transform.position;
+
+        if (moveHorizontal)
+        {
+            newPos.x += directionX * moveSpeed * Time.deltaTime;
+            if (Mathf.Abs(newPos.x - startPosition.x) >= moveRange)
+            {
+                directionX *= -1;
+            }
+        }
+
+        if (moveVertical)
+        {
+            newPos.y += directionY * moveSpeed * Time.deltaTime;
+            if (Mathf.Abs(newPos.y - startPosition.y) >= moveRange)
+            {
+                directionY *= -1;
+            }
+        }
+
+        transform.position = newPos;
+
+        // Rotação
+        transform.Rotate(Vector3.up, 180 * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Bullet"))
+        {
+            health--;
+
+            if (health <= 0)
+            {
+                if (playerShooter != null)
+                {
+                    playerShooter.AddScore(pointsValue);
+                }
+
+                Destroy(other.gameObject);
+                Destroy(gameObject);
+            }
+            else
+            {
+                Destroy(other.gameObject);
+            }
+        }
+    }
+}
